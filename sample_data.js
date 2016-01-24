604dae698ae43a67db5f985b5937a09f408232c6
Adam Sidiali <adamsidiali@gmail.com>
2016-01-24 10:33:21 -0800

    new folder structure

---
+++ sample_data.js
@@ -0,0 +1,18 @@
+SampleData = [
+    {
+        created_at: new Date(),
+        type: "thought",
+        content: "this was my first thought in milestones..."
+    },
+    {
+        created_at: new Date(),
+        type: "task",
+        content: "get milk"
+    },
+    {
+        created_at: new Date(),
+        type: "event",
+        content: "dinner with Ruby",
+        event_time: "Wed Mar 25 2015 01:00:00 GMT-0700"
+    }
+]
